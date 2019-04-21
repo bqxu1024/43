f805344cf855cdccb729f7a88b79bdb6d24620ac
bqxu1024 <34291175+bqxu1024@users.noreply.github.com>
2019-04-21 21:34:54 +0800

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>      //使用cin,cout，须调用iostream 库，否则编译出错
+//#include<cstdlib>         // 使用system( )调用cstdlib库
+//using namespace std;  //在C语言中要省略，例如在VC++和TC++中
+//int main()                      //有的C语言可用void main(),例如TC++和VC++
+//{
+//	cout<<"Hello  World!"<<endl;   //输出"Hello World!" 
+//	system("pause");     //暂停作用，使用system("pause")调用cstdlib库
+//	return 0;                  //结束整个程序
+//}
+
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+	cout << "Hello,World " <<endl;
+	system("pause");
+	return 0;
+}
